cbc8f25dfbfc2c7942bd6335ac39fe80f230ee62
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-02-12 01:58:10 +0200

    Backend <> Testing update transcations

--- backend/test_app.py
+++ backend/test_app.py
@@ -13,6 +13,12 @@ new_transaction_data = {
     'status': 'Pending',
     'amount': 100.00
 }
+updated_transaction_data = {
+    'name': 'Grocery Shopping',
+    'date': '2024-01-01',
+    'status': 'Completed',
+    'amount': 200.00 
+}
 
 class BackendTestCase(unittest.TestCase):
 
@@ -38,7 +44,7 @@ class BackendTestCase(unittest.TestCase):
         
         mock_get_all_transactions.assert_called_once()
 
-    @patch('routes.add_transaction') # Correct indentation
+    @patch('routes.add_transaction') 
     def test_add_new_transaction(self, mock_add_transaction):
         mock_add_transaction.return_value = {**new_transaction_data, 'id': 3}
         
@@ -53,6 +59,21 @@ class BackendTestCase(unittest.TestCase):
         self.assertEqual(data, {**new_transaction_data, 'id': 3})
         
         mock_add_transaction.assert_called_once_with(new_transaction_data)
+
+    @patch('routes.update_transaction')
+    def test_update_existing_transaction(self, mock_update_transaction):
+        mock_update_transaction.return_value = {**updated_transaction_data, 'id': 1}
+        
+        response = self.app.put('/api/transactions/1', json=updated_transaction_data)
+        
+        data = json.loads(response.get_data(as_text=True))
+        
+        self.assertEqual(response.status_code, 200)
+        
+        self.assertEqual(data, {**updated_transaction_data, 'id': 1})
+        
+        mock_update_transaction.assert_called_once_with(1, updated_transaction_data)
+
 
 if __name__ == '__main__':
     unittest.main()
