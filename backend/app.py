7571844913e1d66ea580a35c49ebeac5d7e63ce9
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-02-11 18:03:33 +0200

    Backend <> Updating main app

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from routes import api
+import logging
+from utils import not_found
+
+logging.basicConfig(level=logging.DEBUG)
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+app.register_blueprint(api, url_prefix='/api')
+
+app.register_error_handler(404, not_found)
 
 if __name__ == '__main__':
     app.run(debug=True)
