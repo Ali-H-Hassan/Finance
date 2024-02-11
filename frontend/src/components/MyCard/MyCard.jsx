6d0922944cc67dacc993242162aacfda623574a4
Ali-H-Hassan <ali.7assan.a7@gmail.com>
2024-02-11 16:17:55 +0200

    Frontend <>

---
+++ frontend/src/components/MyCard/MyCard.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./MyCard.css";
+
+function MyCard() {
+  return (
+    <div className="my-card">
+      <h1>My Card</h1>
+      <p>Keep track your financial plan</p>
+
+      <div className="card-container">
+        <div className="card-preview">
+          {/* Card image or design here */}
+          <div className="card-name">ADRIAN TRA</div>
+        </div>
+        <div className="card-details">
+          <h2>Card Information</h2>
+          <div className="card-info">
+            <span>Card No.</span>
+            <span>4889 9271 1937 1932</span>
+          </div>
+          <div className="card-info">
+            <span>Expiry date</span>
+            <span>12/28</span>
+          </div>
+          <div className="card-info">
+            <span>CVV (3-digit security code)</span>
+            <span>***</span>
+          </div>
+          <button className="personalize-button">
+            See how we can personalize your account:
+          </button>
+        </div>
+      </div>
+
+      <div className="help-links">
+        <h2>See Our Help</h2>
+        {/* List of help links */}
+        <ul>
+          <li>Check Your Cash Flow</li>
+          <li>Change Primary Card</li>
+          <li>Pay Tax</li>
+          <li>Make Invoice</li>
+          <li>Refer a Friend</li>
+          <li>Look More</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default MyCard;
